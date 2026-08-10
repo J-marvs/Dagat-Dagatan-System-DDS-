adc71b9463ae37c5517df0ffcfb6b0fcf11f7c06
John Carlo Aguilar <johncarlo.aguilar104@gmail.com>
2026-08-10 18:44:09 +0800

    This is the update rename file

--- server/database/migrations/2026_08_10_081700_create_booking_activities_table.php
+++ server/database/migrations/2026_08_10_081700_create_booking_activities_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->decimal('subtotal', 10, 2);
             $table->string('id_number');
             $table->timestamps();
-            $table->timestamps();
         });
     }
 
